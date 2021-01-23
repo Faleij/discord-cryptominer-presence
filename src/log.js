d3acbe6322ff3f35925c857762075375042e9f82
Faleij <faleij@live.se>
2021-01-23 15:03:42 +0100

    Make log filename human readable

--- src/log.js
+++ src/log.js
@@ -1,6 +1,7 @@
 const fs = require('fs/promises');
 
-const logfile = `${Date.now()}.log`;
+const d = new Date();
+const logfile = `${d.getFullYear()}${(d.getMonth()+1).toString().padStart(2, '0')}${d.getDate().toString().padStart(2, '0')}_${d.getHours().toString().padStart(2, '0')}${d.getMinutes().toString().padStart(2, '0')}${d.getSeconds().toString().padStart(2, '0')}.log`;
 
 const log = (() => {
     let promise = Promise.resolve();
