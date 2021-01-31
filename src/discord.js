2ecfcc229e3dc2a3c6aa1506a0d093dc58a3e476
Faleij <faleij@live.se>
2021-01-31 10:51:08 +0100

    Auto reconnect to discord (resolve #1)

--- src/discord.js
+++ src/discord.js
@@ -3,6 +3,16 @@ const settings = require('./settings');
 
 const rpc = new DiscordRPC.Client({ transport: 'ipc' });
 
-rpc.login({ clientId: settings.discord.clientId || '801823750495666237' });
+function connect() {
+    try {
+        rpc.login({ clientId: settings.discord.clientId || '801823750495666237' });
+    } catch(err) {
+        console.error(err);
+        setTimeout(connect, 1000*3);
+    }
+}
+
+rpc.on('disconnected', connect);
+connect();
 
 module.exports = rpc;
